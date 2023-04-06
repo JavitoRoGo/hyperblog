d683603d36f87f428bb718e4ed8a607516f3e56c
Javi RG <javirgomez@hotmail.com>
2023-04-06 16:48:08 +0100

    Primera versión del .swift

---
+++ misPruebas.swift
@@ -0,0 +1,10 @@
+// Archivo de Swift para hacer mis pruebas
+// Voy a poner código de SwiftUI porque me lo dijo Yago
+
+import SwiftUI
+
+struct MiVista: View {
+    var body: some View {
+        Text("Hola Mundo")
+    }
+}
